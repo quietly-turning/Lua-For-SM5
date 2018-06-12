91291941b7ed65c69f50a5af989b63083f1115c0
Dan Guzek <dguzek@gmail.com>
2018-06-12 12:12:07 -0400

    attempt to fix base url issue
    Following along with: https://itnext.io/so-you-want-to-host-your-single-age-react-app-on-github-pages-a826ab01e48

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 
 ReactDOM.render((
-	<BrowserRouter>
+	<BrowserRouter basename={process.env.PUBLIC_URL}>
 		<App />
 	</BrowserRouter>
 ),  document.getElementById("root"));
