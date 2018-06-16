b217d58208e93537f6025da3b409610299153d09
Dan Guzek <dguzek@gmail.com>
2018-06-16 13:52:41 -0400

    update homepage text

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,17 +8,16 @@ class Home extends Component {
 				<h1>Lua for StepMania 5</h1>
 
 				<p>
-					Hello there!  In these pages, you&apos;ll find my <strong>Lua for SM5</strong> guide, comprised of several chapters each with multiple sub-sections.
+					Hello there!  In these pages, you&apos;ll find my <strong>Lua for SM5</strong> guide, comprised of various chapters organized under the headers <span className="d-none d-md-inline d-lg-inline d-xl-inline">on the right</span> <span className="d-xl-none d-lg-none d-md-none">at the bottom</span> of this page.
 				</p>
 				<p>
-					Please note that this is not exhaustive documentation on every feature StepMania 5 offers.
-					To the best of my knowledge, no such resource exists, though this guide aims to make it easier to get started with SM5 Lua scripting.
+					If you&apos;re unsure of where to start, check out the chapter on <Link to="/Foreword">What Are Actors?</Link> to get a sense of how Lua files are organized in StepMania 5.
 				</p>
 				<p>
-					The closest thing to proper API documentation that SM5 currently has is its Lua.xml file, which comes with StepMania 5 in the <samp>./Docs/Luadoc/</samp> directory and can be viewed in most web browsers. I have also hosted a copy as part of this website: <a href="/Lua-For-SM5/API/Lua.xml">Lua For SM5: Lua API</a>.
+					StepMania 5&apos;s complete API documentation can be found in its Lua.xml file, which ships with releases of StepMania 5 in the <samp>./Docs/Luadoc/</samp> directory and can be viewed in most web browsers. I have also hosted a copy as part of this website: <a href="/Lua-For-SM5/API/Lua.xml">Lua For SM5: Lua API</a>.
 				</p>
 				<p>
-					You can find further resources for learning about Lua and SM5 scripting on the <Link to="/Resources">Other Resources</Link> page.
+					You can find additional resources for learning about Lua and SM5 scripting on the <Link to="/Resources">Other Resources</Link> page.
 				</p>
 				<p>
 					-Dan Guzek
