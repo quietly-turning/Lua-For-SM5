c83980554d73d9c875ede5e45172c8ce3103a104
Dan Guzek <dguzek@gmail.com>
2018-07-23 18:36:39 -0400

    expand on XML vs Lua section
    The Lua lesson now provides an additional example demonstrating a difference between SM3.95's XML and SM5's Lua.

--- src/introduction/Lua.jsx
+++ src/introduction/Lua.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Highlight from 'react-highlight';
+import { Link } from "react-router-dom";
 
 class Lua extends Component {
 	render() {
@@ -134,6 +135,76 @@ ExampleAF[#ExampleAF + 1] = Def.Sprite{
 return ExampleAF
 `}
 			</Highlight>
+
+			<h3>Dynamically Adding Actors in SM5</h3>
+			<p>We can take advantage of the idea that SM5 uses the Lua rather than XML to dynamically add actors to an ActorFrame.  Depending on the scenario and the number of Actors being added to an ActorFrame, this can <em>greatly</em> reduce the lines of code you&apos;ll need to write.</p>
+
+			<p>Imagine that you have a single png asset, <em>dog.png</em> and you want five instances of it to appear on the screen. You&apos;d like the end result to look like this:</p>
+
+			<p><img alt="" className="img-fluid" src="/Lua-For-SM5/img/dogs.png" /></p>
+
+	  		<p>The basic idea is the same regardless of whether you are using SM3.95 or SM5: you&apos;ll need to define five <Link to="/Def.Sprite">Sprite Actors</Link> within your main ActorFrame.  To achieve this in SM3.95&apos;s XML, you could do something like this:</p>
+
+			<span className="CodeExample-Title">Five Dogs in XML:</span>
+			<Highlight className="xml">
+{`<ActorFrame OnCommand="sleep,9999">
+    <children>
+	    <Sprite
+			Name="sprite 1"
+			Texture="./dog.png"
+			InitCommand="x,50;y,50"
+	    />
+	    <Sprite
+			Name="sprite 2"
+			Texture="./dog.png"
+			InitCommand="x,150;y,150"
+	    />
+	    <Sprite
+			Name="sprite 3"
+			Texture="./dog.png"
+			InitCommand="x,250;y,250"
+	    />
+	    <Sprite
+			Name="sprite 4"
+			Texture="./dog.png"
+			InitCommand="x,350;y,350"
+	    />
+	    <Sprite
+			Name="sprite 5"
+			Texture="./dog.png"
+			InitCommand="x,450;y,450"
+	    />
+	</children>
+</ActorFrame>
+`}
+			</Highlight>
+
+			<p>That&apos;s about 30 lines of code already to basically do nearly the same thing five times.  If you&apos;re thinking that a <strong>loop</strong> might be handy here, you&apos;re on <em>exactly</em> the right track!  Let&apos;s rewrite that XML to be Lua for SM5.</p>
+
+			<span className="CodeExample-Title">Five Dogs in Lua:</span>
+			<Highlight className="lua">
+{`local af = Def.ActorFrame{
+	OnCommand=function(self) self:sleep(999) end
+}
+
+-- loop from 0 to 4 and add a Sprite Actor to the af ActorFrame each time
+for i=0,4 do
+	af[#af+1] = LoadActor("./dog.png")..{
+		Name="sprite " .. i+1,
+		InitCommand=function(self)
+			self:xy(i*100+50, i*100+50)
+		end
+	}
+end
+
+return af
+`}
+			</Highlight>
+
+			<p>We were able to accomplish the same effect using a <strong>numerical for loop</strong> in SM5&apos;s Lua in only 15 lines!</p>
+
+			<p>This is a modest improvement already, but imagine if we wanted to add 100 or even 1000 instances of <em>dog.png</em> to the screen.  With XML, it is necessary to explicitly define each Actor you want in your main ActorFrame, while with Lua, it is only necessary to increase the loop&apos;s stop value from <code>4</code> to a different number.</p>
+
 		</div>
 		);
 	}
