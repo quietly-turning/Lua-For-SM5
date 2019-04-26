3217e57028854102dc570f5899af0bbd8871bd16
Dan Guzek <dguzek@gmail.com>
2019-04-26 16:13:05 -0400

    Updates

---
+++ precache-manifest.b92e8dc8012a407e34bc5e6d25c22f22.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b06ec393ed74a8a6dbcbec9ccbeb9de3",
+    "url": "/Lua-For-SM5/index.html"
+  },
+  {
+    "revision": "db128a026835714289f6",
+    "url": "/Lua-For-SM5/static/css/2.7b52c558.chunk.css"
+  },
+  {
+    "revision": "e1f0bb4e263fac3e3764",
+    "url": "/Lua-For-SM5/static/css/main.901046e1.chunk.css"
+  },
+  {
+    "revision": "db128a026835714289f6",
+    "url": "/Lua-For-SM5/static/js/2.52151365.chunk.js"
+  },
+  {
+    "revision": "e1f0bb4e263fac3e3764",
+    "url": "/Lua-For-SM5/static/js/main.ebe55427.chunk.js"
+  },
+  {
+    "revision": "bd28101afe14700a7bcf",
+    "url": "/Lua-For-SM5/static/js/runtime~main.635c4d4a.js"
+  },
+  {
+    "revision": "190e72572ee84190f4777765fa49fc47",
+    "url": "/Lua-For-SM5/static/media/octicons.190e7257.svg"
+  },
+  {
+    "revision": "d038ccbc4a99be24f33a54b482b2422e",
+    "url": "/Lua-For-SM5/static/media/octicons.d038ccbc.eot"
+  },
+  {
+    "revision": "de59a97248b44599e6747a27a943f738",
+    "url": "/Lua-For-SM5/static/media/octicons.de59a972.woff2"
+  },
+  {
+    "revision": "e0d4a324833e13be7d4fa762146d0a71",
+    "url": "/Lua-For-SM5/static/media/octicons.e0d4a324.ttf"
+  },
+  {
+    "revision": "ee5b1bee959a95bd43b223ec901d098a",
+    "url": "/Lua-For-SM5/static/media/octicons.ee5b1bee.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/Lua-For-SM5/precache-manifest.7280cdc58110fde5fe686f1b54f745bc.js"
+  "/Lua-For-SM5/precache-manifest.b92e8dc8012a407e34bc5e6d25c22f22.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Lua-For-SM5/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Lua-For-SM5/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
