fcefa4b6d17194d3ce5df3965d7c2f91e188c021
Dan Guzek <dguzek@gmail.com>
2019-04-26 17:35:32 -0400

    fix Bootstrap/Popper/jQuery import order
    jQuery needs to be imported before Bootstrap's JS.  I followed along with: https://blog.logrocket.com/how-to-use-bootstrap-with-react-a354715d1121

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import $ from 'jquery'
+import Popper from 'popper.js'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
 import React from "react"
 import ReactDOM from "react-dom"
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.css'
 import App from "./App"
 import { BrowserRouter } from "react-router-dom"
 
