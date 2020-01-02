ae80be56b1b3f2a245988adccf86d7f146f58721
Dan Guzek <dguzek@gmail.com>
2020-01-02 13:26:44 -0500

    remove unused imports from index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import $ from 'jquery'
-import Popper from 'popper.js'
-import 'bootstrap/dist/js/bootstrap.bundle.min'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min"
 
 import React from "react"
 import ReactDOM from "react-dom"
-import App from "./App"
 import { BrowserRouter } from "react-router-dom"
+import App from "./App"
+
 
 
 ReactDOM.render((
