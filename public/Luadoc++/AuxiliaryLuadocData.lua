ce01f4125d33b8e3c0dac84f41494e20c2649663
quietly-turning <quietly.turning@gmail.com>
2020-05-23 11:05:59 -0400

    clarify script to generate csv data
    It would be neat to roll this into the engine someday, but this script
    works for now.

---
+++ public/Luadoc++/AuxiliaryLuadocData.lua
@@ -0,0 +1,50 @@
+-- ------------------------------------------
+-- generate auxiliary csv files that can enhance the Lua API docs
+
+-- ------------------------------------------
+local global_functions = function()
+	local s = ""
+
+	for k,v in pairs(_G) do
+		if type(v)=="function" then
+			local info = debug.getinfo(v)
+
+			-- skip global functions from C; line numbers aren't available via debug.getinfo
+			if info.short_src ~= "[C]" then
+				local url = URLEncode( ("%s#L%s-L%s"):format(info.short_src, info.linedefined, info.lastlinedefined) )
+				s = s .. ("%s,%s\n"):format( k, url )
+			end
+		end
+	end
+
+	-- return csv data as string
+	return s
+end
+
+-- local actor_classes = function()
+-- end
+-- ------------------------------------------
+
+-- file path as key                 function as value
+local files = {
+	[theme.."GlobalFunctions.csv"] = global_functions,
+	-- [theme.."ActorClasses.csv"]    = actor_classes,
+}
+
+local theme = THEME:GetCurrentThemeDirectory()
+local file  = RageFileUtil.CreateRageFile()
+
+for path, func in pairs(files) do
+	if file:Open(path, 2) then
+		file:Write( func() )
+
+	else
+		local fError = file:GetError()
+		SCREENMAN:SystemMessage("uh oh.")
+		Trace( "[FileUtils] Error writing to ".. path ..": ".. fError )
+		file:ClearError()
+	end
+end
+
+file:destroy()
+-- ------------------------------------------
