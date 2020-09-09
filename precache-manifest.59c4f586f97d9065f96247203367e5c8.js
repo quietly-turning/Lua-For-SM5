06f75fb3c07d2db7d5a45d39a9762c9bfe7b0aa4
quietly-turning <quietly.turning@gmail.com>
2020-09-09 17:46:11 -0400

    Updates

--- precache-manifest.59c4f586f97d9065f96247203367e5c8.js
+++ precache-manifest.59c4f586f97d9065f96247203367e5c8.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "62bba4421616da60691730d29a874e48",
+    "revision": "c4e703a2ac5407583e51a7892b01c49a",
     "url": "/Lua-For-SM5/index.html"
   },
   {
-    "revision": "2cf40425f01f9a190067",
+    "revision": "059e8c2798e0bed4f36a",
     "url": "/Lua-For-SM5/static/css/2.8e3f952b.chunk.css"
   },
   {
-    "revision": "3cc1caf4d88228dd70cb",
+    "revision": "96f8ab5e63203669f923",
     "url": "/Lua-For-SM5/static/css/main.467bac9d.chunk.css"
   },
   {
-    "revision": "2cf40425f01f9a190067",
-    "url": "/Lua-For-SM5/static/js/2.14a709cd.chunk.js"
+    "revision": "059e8c2798e0bed4f36a",
+    "url": "/Lua-For-SM5/static/js/2.c3ae836b.chunk.js"
   },
   {
     "revision": "126be2fe1a704d28fa040dc5b938723c",
-    "url": "/Lua-For-SM5/static/js/2.14a709cd.chunk.js.LICENSE.txt"
+    "url": "/Lua-For-SM5/static/js/2.c3ae836b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3cc1caf4d88228dd70cb",
-    "url": "/Lua-For-SM5/static/js/main.af124383.chunk.js"
+    "revision": "96f8ab5e63203669f923",
+    "url": "/Lua-For-SM5/static/js/main.69d53077.chunk.js"
   },
   {
     "revision": "e22e8e8b7e980e13a6e1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Lua-For-SM5/precache-manifest.13a61d4ad071613de973b7ae334fc63b.js"
+  "/Lua-For-SM5/precache-manifest.59c4f586f97d9065f96247203367e5c8.js"
 );
 
 self.addEventListener('message', (event) => {
