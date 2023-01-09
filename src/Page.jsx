11eba712daf98455fb5502a44217b790e27d25c8
quietly-turning <quietly.turning@gmail.com>
2023-01-09 17:51:06 -0500

    fix internal linking in Guide

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect }  from "react"
 import { Link, useLocation } from "react-router-dom"
-import hljs            from "highlight.js"
+import hljs  from "highlight.js"
 import parse from 'html-react-parser'
 
 import content from "./page-content.js"
@@ -8,14 +8,12 @@ import content from "./page-content.js"
 function Page(){
 	let location = useLocation()
 
-	const transform = function(node, index){
-		// transform internal <a> elements to react <Link> elements
+	const transform = function(node){
+		// transform internal <a data-component="Link"> elements to react <Link> elements
+		// using html-react-parser's parse() method with the `replace` option
 		if (node.type==="tag" && node.name==="a" && node.attribs && node.attribs["data-component"]==="Link"){
-			return <Link key={index} to={node.attribs.href}>{node.children[0].data}</Link>
+			return <Link to={node.attribs.href}>{node.children[0].data}</Link>
 		}
-
-		// if undefined is returned from transform(), the default behavior of
-		// ReactHtmlParser() is to just act as a pass-through for this node
 	}
 
 	// on url change
@@ -30,7 +28,7 @@ function Page(){
 
 	}, [location.pathname]) // only re-render if url's path changes
 
-	return <div>{ parse(content[location.pathname], {transform: transform}) }</div>;
+	return <div>{ parse(content[location.pathname], {replace: transform}) }</div>;
 }
 
 export default Page
