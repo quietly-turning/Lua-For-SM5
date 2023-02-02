94b9bf652741d0f5169741a6b06a17ae4b56d7b6
quietly-turning <quietly.turning@gmail.com>
2023-02-01 19:43:46 -0500

    Updates

--- Luadoc++/AuxiliaryLuadocData.lua
+++ Luadoc++/AuxiliaryLuadocData.lua
@@ -20,43 +20,29 @@ local sm_objs = {
 }
 
 -- ------------------------------------------
--- ugly
--- TODO: find a Lua library for JSON output
 
 local function_definitions = function()
 
-   local first_section_done = false
-   local json = "{"
-
+   local data = {}
+   
    for section, obj in pairs(sm_objs) do
-      if first_section_done then json = json.."," end
-      first_section_done = true
-
-      json = json .. ("\n\t\"%s\": {"):format(section)
-
-      local first_func_done = false
+      local t = {}
 
       for k,v in pairs(obj) do
-         if type(v)=="function" then
+         if type(v) == "function" then
             local info = debug.getinfo(v)
 
             -- skip functions definined in-engine; line numbers aren't available via debug.getinfo
             if info.short_src ~= "[C]" then
-               if first_func_done then json = json.."," end
-               first_func_done = true
-
                local url = URLEncode( ("%s#L%s-L%s"):format(info.short_src, info.linedefined, info.lastlinedefined) )
-               json = json .. ("\n\t\t\"%s\": \"%s\""):format( k, url )
+               table.insert(t, url)
             end
          end
       end
-      json = json .. "\n\t}"
+      table.insert(data, t)
    end
 
-   json = json .. "\n}"
-
-   -- return json data as string
-   return json
+   return data
 end
 -- ------------------------------------------
 
@@ -65,7 +51,7 @@ local file  = RageFileUtil.CreateRageFile()
 local path  = theme .. "FunctionDefs.json"
 
 if file:Open(path, 2) then
-   file:Write( function_definitions() )
+   file:Write(JsonEncode(function_definitions()))
 
 else
    local fError = file:GetError()
