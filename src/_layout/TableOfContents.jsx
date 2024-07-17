f7891ac4be3d4a5e9c76a45460d466d1fece51a3
quietly-turning <quietly.turning@gmail.com>
2024-07-17 14:51:49 -0400

    don't show empty ToC if no h2 els on page

--- src/_layout/TableOfContents.jsx
+++ src/_layout/TableOfContents.jsx
@@ -23,6 +23,21 @@ function TableOfContents(props){
     return <li key={'section'+i}><a href={'#' + header.id}> {header.text}</a></li>
   })
 
+  const el = () => {
+    if (props.toc?.length <= 0){
+      return
+    }
+
+    return (
+      <div id="toc-container" className="bd-toc col-lg-2 me-2 d-lg-block d-none">
+        <strong className="d-block h6 my-2 pb-2 border-bottom">On this page</strong>
+        <nav id="TableOfContents">
+          {tableOfContents}
+        </nav>
+      </div>
+    )
+  }
+
   useEffect(() => {
     scroll_window_after_hashchange()
   },[window.location.hash])
@@ -30,12 +45,7 @@ function TableOfContents(props){
   return (
     <Routes>
       <Route path="/:group/:page" element={
-        <div id="toc-container" className="bd-toc col-lg-2 me-2 d-lg-block d-none">
-          <strong className="d-block h6 my-2 pb-2 border-bottom">On this page</strong>
-          <nav id="TableOfContents">
-            {tableOfContents}
-          </nav>
-        </div>
+        el()
       } />
     </Routes>
   )
