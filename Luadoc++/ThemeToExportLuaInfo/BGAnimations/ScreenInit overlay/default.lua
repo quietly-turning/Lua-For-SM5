c286e2fc4af4397ce1ae433c3f550e912294584f
quietly-turning <quietly.turning@gmail.com>
2025-07-23 02:07:10 -0400

    Updates

--- Luadoc++/ThemeToExportLuaInfo/BGAnimations/ScreenInit overlay/default.lua
+++ Luadoc++/ThemeToExportLuaInfo/BGAnimations/ScreenInit overlay/default.lua
@@ -1,9 +1,48 @@
 -- ------------------------------------------
--- generate an auxiliary json file that can enhance the Lua API docs
+-- author:  quietly-turning
+-- GitHub:  https://github.com/quietly-turning/Lua-For-SM5
+--
+-- purpose: Generates a json file to enhance the online Lua API docs at
+--          https://quietly-turning.github.io/Lua-For-SM5/LuaAPI
+--          by providing URLs to ITGmania's GitHub, linking readers the exact line-number
+--          where each Global Function in the _fallback theme is defined.
+--
+-- notes:   This file uses `JsonEncode()`, added to ITGmania, to write a json file to disk.
+--          `JsonEncode()` is not available in vanilla SM5, and may not be available in
+--           analogous forks of SM5, but it should be easy to replace with any generic library
+--           that serializes a JSON string from a Lua table.
+--
+-- usage:   Move this theme folder (ThemeToExportLuaInfo) into ./ITGmania/Themes
+--          then use ITGmania's operator menu to switch to this theme.
+--          When this theme reaches ScreenInit (this file), it will write a file to disk like
+--             ./ITGmania/Themes/ThemeToExportLuaInfo/v1.1.0.json
+--          which you can move to
+--             ./Lua-For-SM5/public/Luadoc++/ITGmania/v1.1.0.json
+--
+--          When switching from one theme (e.g. Simply Love) to another (e.g. ThemeToExportLuaInfo)
+--          Global Variables created by the 1st theme will carry over into the 2nd theme and, in
+--          this case, be written to disk in the json file.
+--
+--          My own approach with the Lua-For-SM5 project has been to *only* document functions available
+--          to every ITGmania theme, meaning I don't want to document functions specific to Simply Love.
+--
+--          This requires an export-workflow like:
+--          1.  Start ITGmania.  It starts up with your normal theme, which has its own global functions.
+--          2.  Use the operator menu to switch to ThemeToExportLuaInfo.
+--              A json file is written to disk with global variables from both themes.  Ignore it.  It
+--              will be overwritten in a moment.
+--          3.  Quit ITGmania.
+--          4.  Start ITGmania.  This time, it will start up with ThemeToExportLuaInfo, which contains
+--              no global variables of its own.  A json file is written to disk with global variables from
+--              _fallback.
+--          5.  Move the new json file to your Lua-For-SM5 project.
+--          6.  Use the operator menu to switch back to your normal theme.
+--
 --
 -- for an example of the json file this ITGmania theme generates
 -- see: https://github.com/quietly-turning/Lua-For-SM5/blob/src/public/Luadoc%2B%2B/ITGmania/v1.0.2.json
 
+-- ------------------------------------------
 -- ITGmania objects to inspect for theme-side function defs
 -- hardcoded keys for now (how to traverse Def userdata?)
 local sm_objs = {
